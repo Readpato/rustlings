6f0b4a86dd6d66175f544ac7ea86ea938543729a
Patrick Raedler <code@readpato.dev>
2025-04-21 14:52:41 +0200

    chore: complete exercises

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,4 +1,8 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
+//
+fn call_me() {
+    println!("Hey there, i'm calling you!");
+}
 
 fn main() {
     call_me(); // Don't change this line
